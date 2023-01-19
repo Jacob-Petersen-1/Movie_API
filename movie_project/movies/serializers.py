a6d89c4604a553bcc685ca87870bed8a6fbcef69
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2023-01-19 15:50:01 -0700

    Error in the serializer.py running debug currently

--- movie_project/movies/serializers.py
+++ movie_project/movies/serializers.py
@@ -4,4 +4,4 @@ from .models import Movie
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ['id','title','release_date','description','rating']
+        fields = '__all__'
--- movie_project/movies/views.py
+++ movie_project/movies/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import MovieSerializer
+from .models import Movie
+from django.shortcuts import get_object_or_404
+
+
+class MoviesList(APIView):
+
+    def get(self,request):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies,many=True)
+        return Response(serializer.data, status=status.HTTP_418_IM_A_TEAPOT)
+
+
